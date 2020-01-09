6ec7e828f1e8ffbb1887fdffe9fdda9e4a264d45
Vishnu <visnkmr@gmail.com>
2020-01-09 14:07:17 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 
-var myvar = '<script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-post="vishnunkmr/18" data-width="100%"></script>';
+var myvar = '<script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-post="vishnunkmr/24" data-width="100%"></script>';
 document.write(myvar);
