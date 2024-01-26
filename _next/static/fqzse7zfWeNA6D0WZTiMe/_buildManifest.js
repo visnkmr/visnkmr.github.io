aa06bfd571938daea8494478c91230c19a05fd29
fromghrunner <12533753+visnkmr@users.noreply.github.com>
2024-01-26 20:35:05 +0000

    based on latest release on https://github.com/visnkmr/homepagev2

---
+++ _next/static/fqzse7zfWeNA6D0WZTiMe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-47ba40762e6d43f2.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
