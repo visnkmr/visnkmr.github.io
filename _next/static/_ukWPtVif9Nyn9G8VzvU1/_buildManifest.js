634ebfdf48436ec5e86a0edfb048e64f86c23f7e
fromghrunner <12533753+visnkmr@users.noreply.github.com>
2025-02-22 13:50:00 +0000

    based on latest release on https://github.com/visnkmr/homepagev2

---
+++ _next/static/_ukWPtVif9Nyn9G8VzvU1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2f6e8263dd91fe7a.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ content.js
@@ -0,0 +1,3 @@
+
+var myvar = '<script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-post="vishnunkmr/31" data-width="100%"></script>';
+document.write(myvar);
