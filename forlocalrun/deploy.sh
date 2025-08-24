645c3ea67ef84a0afe7d850c52ef05cee29f0f13
fromghrunner <12533753+visnkmr@users.noreply.github.com>
2025-08-24 20:30:36 +0000

    based on latest release on https://github.com/visnkmr/homepagev2

---
+++ forlocalrun/deploy.sh
@@ -0,0 +1,5 @@
+cd ../
+git init
+git add .
+git commit -m "based on latest release on https://github.com/visnkmr/homepagev2"
+git push -f https://git:1868a3b31b7575e30492ad7901fea1bd5e2962d5@codeberg.org/visnk/pages.git main
